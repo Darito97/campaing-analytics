6134b507e8d71698109ffcf60ef6ccf02c52e133
David <davidalonso991@gmail.com>
2025-12-16 19:35:13 -0600

    Agregado de filtros opcionales de fechas a backend y entrega de total y numero de obtenidos

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -8,12 +8,27 @@ def get_campaigns(
     db: Session,
     skip: int = 0,
     limit: int = 10,
-    tipo_campania: Optional[str] = None
+    tipo_campania: Optional[str] = None,
+    start_date: Optional[datetime] = None,
+    end_date: Optional[datetime] = None
 ):
     query = db.query(models.Campaign)
+
     if tipo_campania:
         query = query.filter(models.Campaign.tipo_campania == tipo_campania)
-    return query.offset(skip).limit(limit).all()
+    
+    if start_date and end_date:
+        query = query.filter(
+            and_(
+                models.Campaign.fecha_inicio >= start_date,
+                models.Campaign.fecha_fin <= end_date
+            )
+        )
+    
+    total = query.count()
+    campaings = query.offset(skip).limit(limit).all()
+    
+    return campaings, total
 
 def get_campaign(db: Session, campaign_id: str):
     return db.query(models.Campaign).filter(models.Campaign.name == campaign_id).first()
